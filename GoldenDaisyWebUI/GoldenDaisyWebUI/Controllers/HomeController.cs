8f15eef7448c04c2719c83132be9c9dadf6bdc51
hakansari1308 <73712629+hakansari1308@users.noreply.github.com>
2023-07-18 17:06:57 +0300

    GoldenDaisyBeauty

---
+++ GoldenDaisyWebUI/GoldenDaisyWebUI/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using GoldenDaisyWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GoldenDaisyWebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public IActionResult Randevu()
+        {
+            return View();
+        }
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+        public IActionResult adminlist()
+        {
+            return View();
+        }
+        public IActionResult profil()
+        {
+            return View();
+        }
+        public IActionResult login() {
+        return View();
+        }
+        public IActionResult sign() {
+            return View();
+        }
+        public IActionResult duyuru()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
